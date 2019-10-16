a19cf4134d1f6a214374a107802dab74d26afeb7
Christian Koch <ckoch@cars.com>
2019-10-16 14:48:44 -0400

    add TimSort tests

--- test/timsort_cpp_ex_test.exs
+++ test/timsort_cpp_ex_test.exs
@@ -2,7 +2,17 @@ defmodule TimsortCppExTest do
   use ExUnit.Case
   doctest TimsortCppEx
 
-  test "greets the world" do
-    assert TimsortCppEx.hello() == :world
+  test "sorts a single element list" do
+    assert TimsortCppEx.sort([1]) == [1]
+  end
+
+  test "sorts a two element list" do
+    assert TimsortCppEx.sort([1, 2]) == [1, 2]
+    assert TimsortCppEx.sort([2, 1]) == [1, 2]
+  end
+
+  test "sorts a three element list" do
+    assert TimsortCppEx.sort([1, 2, 3]) == [1, 2, 3]
+    assert TimsortCppEx.sort([2, 1, 3]) == [1, 2, 3]
   end
 end
